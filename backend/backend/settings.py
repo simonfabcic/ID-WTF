5e4921f9e3adf4db48b30e216076159c762b9977
Simon Fabcic <simon.fabcic@gmail.com>
2026-01-11 17:46:19 +0100

    added connection to the MySql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -93,8 +94,15 @@ class Base(Configuration):
 
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("DB_NAME", "django_db"),
+            "USER": os.getenv("DB_USER", "django_user"),
+            "PASSWORD": os.getenv("DB_PASS", "django_pass"),
+            "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+            "PORT": os.getenv("DB_PORT", "3306"),
+            "OPTIONS": {
+                "charset": "utf8mb4",
+            },
         }
     }
 
